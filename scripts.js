fa344d2ccbe10a2693e8df6de2504e38dd75eb1f
Bryan Panaccione <bryan.j.panaccione@gmail.com>
2022-02-02 12:10:04 -0600

    "button higher"

--- scripts.js
+++ scripts.js
@@ -7,35 +7,29 @@ let currentTop = 0;
 let currentLeft = 0;
 const documentHeight = document.documentElement.clientHeight;
 const documentWidth = document.documentElement.clientWidth;
-//eventlistener for catbuttonclick
+//eventlistener for catbuttonclicks
 
 catButtonClick.addEventListener('click', deleteCat)
 catButtonClick.addEventListener('click', addCat)
 
-//addcat function
+//cat functions
 
-function playMeow(){
+function playMeow() {
     var sound = document.getElementById("soundOfCats")
     sound.play()
 }
-function deleteCat(){
+
+function deleteCat() {
     document.querySelectorAll('.Meow').forEach(e => e.remove());
-    //let catsOnScreen = document.querySelectorAll('.Meow')
-    //console.log(catsOnScreen)
-    //catsOnScreen.remove()
-    //for (var i = 0; i < catsOnScreen.length; i++) {
-    //must go back to document with document. to effect the element, otherwise it just clears array
-       //delete catsOnScreen[0];
-    //}
 }
-function addCat(){
-    currentTop = Math.floor(Math.random()*documentHeight)+1;
-    currentLeft = Math.floor(Math.random()*documentWidth)+1;
-    let catSelection = Math.floor(Math.random()*10)
+function addCat() {
+    currentTop = Math.floor((Math.random() * documentHeight) / 2 + (documentHeight / 4)) + 1;
+    currentLeft = Math.floor((Math.random() * documentWidth) / 3 + (documentWidth / 3)) + 1;
+    let catSelection = Math.floor(Math.random() * 10)
     console.log(currentLeft)
-    catStarterDiv.style.top = currentTop+'px';
+    catStarterDiv.style.top = currentTop + 'px';
     console.log(catStarterDiv.style.top)
-    catStarterDiv.style.left = currentLeft+'px';
+    catStarterDiv.style.left = currentLeft + 'px';
     let randomCat = document.createElement('div');
     randomCat.classList.add('Meow');
     randomCat.innerHTML = `<img src="images/catPic${catSelection}.png" alt="cat" width="100" class="catStart">`;
